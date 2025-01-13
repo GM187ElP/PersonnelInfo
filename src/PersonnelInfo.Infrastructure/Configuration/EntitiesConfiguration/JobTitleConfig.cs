3e6c102a96738116f495743739ed1e2f947db536
GM347El <arsalan.akorn@gmail.com>
2025-01-13 11:30:45 +0330

    us3

---
+++ src/PersonnelInfo.Infrastructure/Configuration/EntitiesConfiguration/JobTitleConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PersonnelInfo.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonnelInfo.Infrastructure.Configuration.EntitiesConfiguration;
+public class JobTitleConfig : IEntityTypeConfiguration<JobTitle>
+{
+    void IEntityTypeConfiguration<JobTitle>.Configure(EntityTypeBuilder<JobTitle> builder)
+    {
+        builder.Property(e => e.Title).HasMaxLength(21).IsRequired();
+        builder.HasOne(e=>e.Department).WithMany(e=>e.JobTitles).HasForeignKey(e=>e.DepartmentId);
+    }
+}
+
