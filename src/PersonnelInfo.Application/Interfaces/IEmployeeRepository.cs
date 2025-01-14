ebd0d9c0d3347b8bd76c01aa43cc5afe58ad7a48
GM347El <arsalan.akorn@gmail.com>
2025-01-14 12:27:12 +0330

    us9

---
+++ src/PersonnelInfo.Application/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using PersonnelInfo.Core.Entities;
+
+namespace PersonnelInfo.Application.Interfaces;
+public interface IEmployeeRepository
+{
+    Task AddAsync(Employee entity);
+    Task DeleteByIdAsync(long id);
+    Task UpdateAsync(Employee entity);
+    Task<List<Employee>> GetAllAsync();
+    Task<Employee> GetByIdAsync(long id);
+}
---
+++ src/PersonnelInfo.Infrastructure/Data/Repositories/BankNameRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonnelInfo.Application.Interfaces;
+using PersonnelInfo.Core.Entities;
+using System.Runtime.InteropServices;
+
+namespace PersonnelInfo.Infrastructure.Data.Repositories;
+public class BankNameRepository : IBankNameRepository
+{
+    readonly DbSet<BankName> _dbSet;
+    readonly DbContext _context;
+
+    public BankNameRepository(DbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<BankName>();
+    }
+
+    public async Task AddAsync(BankName entity) => await _dbSet.AddAsync(entity);
+
+    public async Task DeleteByIdAsync(string id) => _dbSet.Remove(await _dbSet.FindAsync(id) ?? throw new NotFoundEntity(typeof(BankName)));
+
+
+    public async Task<List<BankName>> GetAllAsync()
+    {
+        var entities = await _dbSet.AsNoTracking().ToListAsync();
+        if (!entities.Any()) throw new NotFoundEntity(typeof(BankName));
+        return entities;
+    }
+
+    public async Task<BankName> GetByIdAsync(string id) => await _dbSet.AsNoTracking().FirstOrDefaultAsync(e => e.Name == id) ?? throw new NotFoundEntity(typeof(BankName));
+
+    public async Task UpdateAsync(BankName entity) => _context.Entry(await _dbSet.FindAsync(entity.Name) ?? throw new NotFoundEntity(typeof(BankName))).CurrentValues.SetValues(entity);
+}
