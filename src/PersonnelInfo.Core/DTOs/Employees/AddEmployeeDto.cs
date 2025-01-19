b8aacc7f5f87f448ab161734b52a3d151f7022de
unknown <arsalan.akorn@gmail.com>
2025-01-19 23:35:17 +0330

    u

--- src/PersonnelInfo.Core/DTOs/Employees/AddEmployeeDto.cs
+++ src/PersonnelInfo.Core/DTOs/Employees/AddEmployeeDto.cs
@@ -1,4 +1,5 @@
-﻿using PersonnelInfo.Core.Enums;
+﻿using PersonnelInfo.Core.Entities;
+using PersonnelInfo.Core.Enums;
 using System.ComponentModel.DataAnnotations;
 
 public class AddEmployeeDto
@@ -16,56 +17,64 @@ public class AddEmployeeDto
     public string ContactNumber { get; set; }
     #endregion
 
+    
+
     #region Gender and Status
-    public GenderType? GenderDisplay { get; set; }
-    public WorkingStatusType? WorkingStatusDisplay { get; set; }
+    public GenderType GenderDisplay { get; set; } = GenderType.Unknown;
+    public WorkingStatusType WorkingStatusDisplay { get; set; } = WorkingStatusType.Working;
     #endregion
 
     #region Family Information
-    public string FatherName { get; set; }
-    public bool? IsMarried { get; set; }
-    public int? ChildrenCount { get; set; }
+    public string? FatherName { get; set; }
+    public bool IsMarried { get; set; } = false;
+    public int ChildrenCount { get; set; } = 0;
     #endregion
 
     #region Identity Information
-    public string ShenasnameNumber { get; set; }
-    public string ShenasnameSerialLetter { get; set; }
-    public string ShenasnameSerie { get; set; }
-    public string ShenasnameSerial { get; set; }
+    public string? ShenasnameNumber { get; set; }
+    public string? ShenasnameSerialLetter { get; set; }
+    public string? ShenasnameSerie { get; set; }
+    public string? ShenasnameSerial { get; set; }
     #endregion
 
     #region Birth and Place Information
-    public DateTime? BirthDate { get; set; }
-    public int? BirthPlaceId { get; set; }
+    public DateTime BirthDate { get; set; } = DateTime.Now;
+    public long? BirthPlaceId { get; set; }
+    public City? BirthPlace { get; set; }
     #endregion
 
     #region Shenasname Issuance Information
-    public int? ShenasnameIssuedPlaceId { get; set; }
+    public long? ShenasnameIssuedPlaceId { get; set; }
+    public City? ShenasnameIssuedPlace { get; set; }
     #endregion
 
     #region Insurance Information
-    public string InsurranceCode { get; set; }
-    public string InsurranceStatus { get; set; }
+    public string? InsurranceCode { get; set; }
+    public string? InsurranceStatus { get; set; }
     public bool? HasInsurance { get; set; }
-    public int? ExtraInsurranceCount { get; set; }
+    public int ExtraInsurranceCount { get; set; } = 0;
     #endregion
 
     #region Employment Information
-    public int? DepartmentId { get; set; }
-    public EmploymentType? EmploymentTypeDisplay { get; set; }
-    public DateTime LeftDate { get; set; }
-    public string SupervisorName { get; set; }
+    public string? DepartmentId { get; set; }
+    public JobTitle? JobTitle { get; set; }
+    public EmploymentType EmploymentTypeDisplay { get; set; } = EmploymentType.Official;
+    public DateTime StartingDate { get; set; } = DateTime.Now;
+    public DateTime? LeavingDate { get; set; }
+    public long? SupervisorId { get; set; }
     #endregion
 
     #region Contact Information
-    public string? InternalContactNumber { get; set; }
+    public string InternalContactNumber { get; set; } = "000";
     public string? LandPhoneNumber { get; set; }
-    public string Address { get; set; }
-    public string PostalCode { get; set; }
+    public string? Address { get; set; }
+    public string? PostalCode { get; set; }
     #endregion
 
     #region Academic Information
-    public string MostRecentDegree { get; set; }
-    public string Major { get; set; }
+    public string? MostRecentDegree { get; set; }
+    public string? Major { get; set; }
     #endregion
+
+   
 }
